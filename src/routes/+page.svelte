ad70aca377619718d01ea826469e8de7f19042f1
Jhosimar Aguacia <corbin026@hotmail.com>
2023-04-15 16:53:01 +0300

    Create SveltKit project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+    let title = 'Svelte Gaming Guides';
+</script>
+<div class="index">
+	<h2>{title}</h2>
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non molestias earum ducimus, provident
+		sit perspiciatis? Doloribus nisi possimus fuga nulla mollitia aspernatur delectus cumque tenetur
+		asperiores blanditiis? Dolorum, quam? Magnam!
+	</p>
+</div>
+
+<style>
+	.index {
+        text-align: center;
+        display: block;
+        margin: 20px auto;
+    }
+</style>
