80a2eb184a54a34519d8ab198cf7c327a3d02a7b
Jhosimar Aguacia <corbin026@hotmail.com>
2023-04-16 13:36:56 +0300

    Add Title component

---
+++ src/lib/title.svelte
@@ -0,0 +1,23 @@
+<script>
+  export let title = 'Svelte Gaming Default Title';
+</script>
+
+<div class="title">
+  <h1>{title}</h1>
+  <img src="/greek-helmet.png" alt="" />
+</div>
+
+<style>
+  .title {
+    display: flex;
+    flex-direction: column;
+  }
+  .title img {
+    display: inline-block;
+    filter: invert(88%) sepia(18%) saturate(886%) hue-rotate(199deg) brightness(100%) contrast(90%);
+  }
+
+  .title h1 {
+    margin-left: 10px;
+  }
+</style>
