8f24d04f3bc3824b481049308c6a4994e9d667cd
Jhosimar Aguacia <corbin026@hotmail.com>
2023-04-16 14:21:19 +0300

    Add default layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,29 @@
+<script>
+  import '../Styles/global.css';
+  import Title from '$lib/title.svelte';
+  let title = 'Svelte Gaming Guides';
+</script>
+
+<header>
+  <Title {title} />
+</header>
+<main>
+  <slot />
+</main>
+<footer>
+  <p>Copyright 2023 Svelte Gaming Guides</p>
+</footer>
+
+<style>
+  header {
+    display: flex;
+    justify-content: center;
+  }
+  main {
+    max-width: 960px;
+    margin: 20px auto;
+  }
+  footer {
+    text-align: center;
+  }
+</style>
